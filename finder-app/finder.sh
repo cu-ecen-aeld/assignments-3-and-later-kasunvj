2d218f7a0c05ab9a63253494ea29f2e3b6e35413
Martian <kasunvj@gmail.com>
2024-07-07 21:33:28 +0530

    Added finder and writer

---
+++ finder-app/finder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+filesdir=$1
+searchstr=$2
+
+if [ $# -ne 2 ]
+then
+	if [ $# -eq 1 ]
+	then
+		echo "You have missed the 2nd parameter" 
+	elif [ $# -eq 0 ]
+	then
+		echo "You have missed 1st and 2nd parameters"
+	else 
+		echo "Too many paramaters"
+	fi  
+	
+	exit 1
+fi
+
+if [ ! -d "$filesdir" ]
+then
+	echo "Not a directory"
+	exit 1
+fi
+
+nfiles=$(ls $filesdir | wc -l)
+cd "$filesdir"
+nmatch=$(grep -r "$searchstr" * | wc -l)
+
+echo "The number of files are ${nfiles} and the number of matching lines are ${nmatch}"
---
+++ finder-app/writer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+writefile=$1
+writestr=$2
+if [ $# -ne 2 ]
+then
+	if [ $# -eq 1 ]
+	then
+		echo "You have missed the 2nd parameter" 
+	elif [ $# -eq 0 ]
+	then
+		echo "You have missed 1st and 2nd parameters"
+	else 
+		echo "Too many paramaters"
+	fi  
+	
+	exit 1
+fi
+
+dirname=$(dirname "$writefile")
+mkdir $dirname
+
+if [ $? -eq 1 ]
+then
+    mkdir $(dirname $dirname)
+    mkdir $dirname
+fi
+
+echo "$writestr" >> "$writefile"
+
+if [ $? -eq 1 ]
+then 
+    echo "Error creating the file"
+fi
